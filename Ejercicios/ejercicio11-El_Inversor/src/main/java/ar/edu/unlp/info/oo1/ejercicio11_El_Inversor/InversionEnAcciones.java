ef9c785b4f0a8dfdd0f1945841819d507b56e006
FlokiFoodBuilders <lucas.gallardo196304@alumnos.info.unlp.edu.ar>
2022-10-06 21:16:32 -0300

    add eje11

---
+++ Ejercicios/ejercicio11-El_Inversor/src/main/java/ar/edu/unlp/info/oo1/ejercicio11_El_Inversor/InversionEnAcciones.java
@@ -0,0 +1,9 @@
+package ar.edu.unlp.info.oo1.ejercicio11_El_Inversor;
+
+public class InversionEnAcciones implements Inversion {
+	
+	public double valorActual() {
+			
+	}
+	
+}
---
+++ Ejercicios/ejercicio11-El_Inversor/src/main/java/ar/edu/unlp/info/oo1/ejercicio11_El_Inversor/PlazoFijo.java
@@ -0,0 +1,12 @@
+package ar.edu.unlp.info.oo1.ejercicio11_El_Inversor;
+
+public class PlazoFijo  implements Inversion {
+	private String nombre; 
+	private int cantidad; 
+	private 
+	
+	public double valorActual() {
+		
+	}
+	
+}

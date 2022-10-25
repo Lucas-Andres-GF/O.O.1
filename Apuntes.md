f49aa1abbcb4720f702c4f631503ac95061f3fee
FlokiFoodBuilders <lucas.gallardo196304@alumnos.info.unlp.edu.ar>
2022-10-25 19:33:31 -0300

    changes editor

--- Apuntes.md
+++
@@ -1,5 +0,0 @@
-# Orientacion a Objetos 1
-
-- Utilizar el This en el constructor. 
-- hacer estrictamente lo que define el metodo. (Side effect)
-
--- Ejercicios/ejercicio11-El_Inversor/src/main/java/ar/edu/unlp/info/oo1/ejercicio11_El_Inversor/InversionEnAcciones.java
+++
@@ -1,9 +0,0 @@
-package ar.edu.unlp.info.oo1.ejercicio11_El_Inversor;
-
-public class InversionEnAcciones implements Inversion {
-	
-	public double valorActual() {
-			
-	}
-	
-}
